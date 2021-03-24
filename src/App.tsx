c45dee67def858b78bbb69a0b77f77f2483e03c4
odailsonsilva <odafreelancerti@gmail.com>
2021-03-24 07:34:05 -0300

    feat: criando eslizacao global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
-   <h1>ola denovo react</h1>
+   <>
+    <h1>ola denovo react</h1>
+    <GlobalStyle />
+   </>
   );
 }
