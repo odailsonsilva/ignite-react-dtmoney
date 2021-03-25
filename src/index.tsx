3c8e54a448ce1176e544388a245b2eefb71bac03
odailsonsilva <odafreelancerti@gmail.com>
2021-03-25 11:32:13 -0300

    feat: seeds para popular transaction padroes e listando transacoes

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,28 @@ createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de website',
+          category: 'Dev',
+          type: 'deposit',
+          amount: 6000,
+          createAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          category: 'Casa',
+          amount: 1100,
+          type: 'withdraw',
+          createAt: new Date('2021-02-14 09:00:00')
+        }
+      ]
+    })
+  },
   routes() {
     this.namespace = 'api';
 
